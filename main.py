f1d90e9363ba6654df3e6b1cde1af556bedc5592
Woshington Almeida <woshingtonbalmeida@gmail.com>
2024-02-01 17:59:34 -0300

    Meu primeiro commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Jornada de Dados")
+
+#variaveis
+idade = 43
+nome = "Woshington"
+
+#print
+print(idade)
+
+#soma
+nova_idade = idade +1
+print(nova_idade)
+
+#lista
+lista = [1,2,3,4,5]
+
+#metodoss
+lista_invertida = lista.reverse()
